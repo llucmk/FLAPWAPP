9353e37d7ec368c4313e8a3dcda44cdddeea1b96
Lluc <Llucmk@gmail.com>
2018-05-30 23:42:44 +0200

    Tried so many ways to get collision. Still work in progress

--- js/main.js
+++ js/main.js
@@ -15,8 +15,7 @@ var jump = 4;
 var gravity = -0.25;
 
 // Pipes
-var pipeheight = 125; // Separation between pipes
-var pipewidth = 52;
+var pipeheight = 90; // Separation between pipes
 var pipes =  new Array();
 
 /* Variables for Game Framerate and loop updating */
@@ -55,6 +54,94 @@ function gameLoop(){
 
     updateBird(bird);
 
+    var playerHitBox = document.getElementById("bird").getBoundingClientRect();
+
+    var playerBoxtop = (playerHitBox.top + playerHitBox.width) / 2; // Gets the middle-top position of the bird.
+    var playerBoxbottom = (playerHitBox.bottom + playerHitBox.width) / 2; // Gets the middle-top position of the bird.
+    var playerBoxleft = playerHitBox.left;
+    var playerBoxright = playerHitBox.right;
+
+    // debug
+    var boundingbox = $("#playerbox");
+    boundingbox.css('left', playerBoxleft);
+    boundingbox.css('top', playerBoxtop);
+    boundingbox.css('height', playerHitBox.height);
+    boundingbox.css('width', playerHitBox.width);
+
+    // console.log(playerBoxright);
+
+    if (pipes[0] != null) {
+        var lastPipeSel = pipes[0];
+        var nextUpperPipe = lastPipeSel.children(".pipe_upper");
+        // nextUpperPipe.css({"background-color":"red"});
+
+        var pipetop = nextUpperPipe.offset().top + nextUpperPipe.height(); // Gets the top position of the upper pipe and adds the height of the pipe to get the left bottom corner coords
+        var pipebottom = pipetop + pipeheight;
+        var pipeleft = nextUpperPipe.offset().left - 2;
+        var piperight = pipeleft + nextUpperPipe.width();
+
+        // debug
+
+        var boundingbox = $("#pipebox");
+        boundingbox.css('left', pipeleft);
+        boundingbox.css('top', pipetop);
+        boundingbox.css('height', pipeheight);
+        boundingbox.css('width', nextUpperPipe.width());
+
+
+
+        // is the player inside the pipe on x position?
+        if(playerBoxright > pipeleft) {
+            // Is it passing between the top and bottom pipes?
+            if(nextUpperPipe.height() > pipetop) {
+                console.log(nextUpperPipe.height());
+                console.log("Bien!");
+                console.log(playerBoxtop);
+            } else {
+                // The player touched the pipes
+                console.log(nextUpperPipe.height());
+                console.log("bad!");
+                console.log(playerBoxtop);
+                playerDead();
+            }
+        }
+
+        // console.log(playerBoxleft + "  > " + piperight);
+        if(playerBoxleft > piperight) {
+            pipes.splice(0, 1);
+        }
+
+
+        // console.log(nextUpperPipe.width());
+        // console.log(playerHitBox.x + "   " +nextpipe.offset().left);
+
+
+
+        // var upperPipeHbox = lastPipeSel.children(".pipe_upper");
+        // var upperboundingPipeHbox = lastPipeSel.children(".pipe_upper").getBoundingClientRect();
+        // var lowerPipeHbox = lastPipeSel.children(".pipe_lower").getBoundingClientRect();
+        //
+        // var pipetop = upperPipeHbox.offset().top + upperPipeHbox.height();
+        //
+        // // Debugging
+        // console.log("pipeptop: " + pipetop  + " <br /> upperboundingPipeHbox: " + upperboundingPipeHbox.top );
+        // console.log(playerHitBox.right);
+
+        }
+
+        // $(bird).css("background-color", "blue");
+
+        // lastPipeSel[0].style.backgroundColor = "red";
+        // lastPipeSel[0].children[1].style.backgroundColor = "red";
+        // console.log(lastPipeSel[0].children[1]);
+        // lastPipeSel.style.backgroundColor = "red";
+
+    // console.log(pipes);
+    // var upPipeHitBox = document.getElementById("pipe_upper").getBoundingClientRect();
+    // var lowPipeHitBox = document.getElementById("pipe_lower").getBoundingClientRect();
+    //  console.log(pipes);
+
+
 }
 
 function updateBird(bird){
@@ -73,8 +160,8 @@ function updateBird(bird){
 }
 
 function updatePipes() {
-    // Remove any pipe that already passed left position by more than -100.
-    $(".pipe").filter(function() { return $(this).position().left <= -100; }).remove();
+    // Remove any pipe that already passed left position by more than -70.
+    $(".pipe").filter(function() { return $(this).position().left <= -70; }).remove();
 
     //adds a new pipe
     var padding = 60;
